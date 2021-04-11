8287f87948a4f357c8aa3c24be992cf3d8d5324a
desty2k <55806660+desty2k@users.noreply.github.com>
2021-04-11 20:23:33 +0200

    UPD: use combobox for selecting task in task window

--- QtPyBotnet/view/TaskWindow/Widgets.py
+++ QtPyBotnet/view/TaskWindow/Widgets.py
@@ -1,6 +1,6 @@
-from qtpy.QtCore import Signal, Slot
-from qtpy.QtWidgets import (QWidget, QHBoxLayout, QGroupBox, QCheckBox, QLineEdit, QDateTimeEdit,
-                            QTextEdit, QSpinBox, QVBoxLayout, QRadioButton, QFormLayout, QLabel)
+from qtpy.QtCore import Signal, Slot, Qt
+from qtpy.QtWidgets import (QWidget, QHBoxLayout, QGroupBox, QCheckBox, QLineEdit, QComboBox,
+                            QTextEdit, QSpinBox, QVBoxLayout, QRadioButton, QFormLayout, QLabel, QDateTimeEdit)
 
 from core.config import ConfigManager
 
@@ -30,22 +30,25 @@ class SelectionWidget(QGroupBox):
         self.widget_layout = QVBoxLayout(self)
         self.setLayout(self.widget_layout)
         self.setTitle("Tasks")
-        self.radios = []
+
+        self.combo = QComboBox(self)
+        self.combo.currentTextChanged.connect(lambda text: self.selection_changed.emit(self.get_task_by_name(text)))
+        self.widget_layout.addWidget(self.combo)
 
         self.tasks = ConfigManager().get_tasks()
+        for index, task in enumerate(self.tasks):
+            if not task.experimental:
+                self.combo.addItem(task.__name__)
+                self.combo.setItemData(index, task.description, Qt.ToolTipRole)
+
+    def get_task_by_name(self, name):
         for task in self.tasks:
-            radio = QRadioButton(task.__name__, self)
-            radio.clicked.connect(lambda _, t=task: self.selection_changed.emit(t))
-            radio.setToolTip(task.description)
-            self.widget_layout.addWidget(radio)
-            self.radios.append(radio)
+            if task.__name__ == name:
+                return task
 
     @Slot()
     def on_send_button_clicked(self):
-        for radio in self.radios:
-            if radio.isChecked():
-                self.send_task.emit(int(self.bot_edit.value()), str(radio.text()))
-                return
+        self.send_task.emit(int(self.bot_edit.value()), str(self.combo.currentText()))
 
 
 class KwargsWidget(QGroupBox):
@@ -80,41 +83,42 @@ class KwargsWidget(QGroupBox):
 
         kwargs_dict = task.kwargs
         for key, value in kwargs_dict.items():
-            key_label = QLabel(str(key), self)
+            key_label = QLabel(str(key).replace("_", " ").capitalize(), self)
             val_type = value.get("type")
             default = value.get("default")
 
             if val_type is int:
-                widget = QSpinBox(self)
-                widget.setRange(0, 100000)
-                widget.setValue(int(default))
+                value_widget = QSpinBox(self)
+                value_widget.setRange(-1, 100000)
+                value_widget.setValue(int(default))
             elif val_type is str:
-                widget = QLineEdit(self)
-                widget.setText(str(default))
+                value_widget = QLineEdit(self)
+                value_widget.setText(str(default))
             elif val_type is bool:
                 key_label = None
-                widget = QCheckBox(str(key), self)
-                widget.setChecked(bool(default))
+                value_widget = QCheckBox(str(key).replace("_", " ").capitalize(), self)
+                value_widget.setChecked(bool(default))
             elif val_type == "text":
-                widget = QTextEdit(self)
-                widget.setText(str(default))
+                value_widget = QTextEdit(self)
+                value_widget.setText(str(default))
             else:
                 continue
 
-            widget.setObjectName(key)
-            widget.setToolTip(value.get("description"))
-            self.widgets.append({"key": key_label, "value": widget})
-            self.widget_layout.addRow(key_label, widget)
+            value_widget.setObjectName(key)
+            value_widget.setToolTip(value.get("description"))
+            self.widgets.append({"key": key_label, "value": value_widget})
+            self.widget_layout.addRow(key_label, value_widget)
 
     def get_kwargs(self):
         kwargs = {}
-        for widget in self.widgets:
-            if type(widget) == QSpinBox:
-                kwargs[widget.objectName()] = widget.value()
-            elif type(widget) == QLineEdit:
-                kwargs[widget.objectName()] = widget.text()
-            elif type(widget) == QTextEdit:
-                kwargs[widget.objectName()] = widget.toPlainText()
-            elif type(widget) == QCheckBox:
-                kwargs[widget.objectName()] = widget.isChecked()
+        for widget_dict in self.widgets:
+            value_widget = widget_dict.get("value")
+            if type(value_widget) == QSpinBox:
+                kwargs[value_widget.objectName()] = value_widget.value()
+            elif type(value_widget) == QLineEdit:
+                kwargs[value_widget.objectName()] = value_widget.text()
+            elif type(value_widget) == QTextEdit:
+                kwargs[value_widget.objectName()] = value_widget.toPlainText()
+            elif type(value_widget) == QCheckBox:
+                kwargs[value_widget.objectName()] = value_widget.isChecked()
         return kwargs
