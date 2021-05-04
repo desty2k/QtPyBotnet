627f59c0f6aae98767addc2dc4f150b215144664
desty2k <55806660+desty2k@users.noreply.github.com>
2021-05-04 12:57:22 +0200

    ADD: payload window and progress window

---
+++ QtPyBotnet/view/PayloadWindow/ProgressWindow.py
@@ -0,0 +1,49 @@
+from qtpy.QtWidgets import QVBoxLayout, QPlainTextEdit
+from qtpy.QtCore import Slot, Qt
+from qtpy.QtGui import QSyntaxHighlighter, QTextCharFormat
+
+
+from qrainbowstyle.windows import FramelessWindow
+
+
+class Highlighter(QSyntaxHighlighter):
+
+    def __init__(self, parent):
+        super(Highlighter, self).__init__(parent)
+        self.generator_format = QTextCharFormat()
+        self.generator_format.setForeground(Qt.yellow)
+
+    def highlightBlock(self, text):
+        if text.startswith('<GENERATOR>'):
+            self.setFormat(0, len(text), self.generator_format)
+
+
+class ProgressWindow(FramelessWindow):
+
+    def __init__(self, parent, generator):
+        super(ProgressWindow, self).__init__(parent)
+        self.__generator = generator
+
+        self.__progress_view = QPlainTextEdit(self)
+        self.__highlighter = Highlighter(self.__progress_view.document())
+        self.__progress_view.textChanged.connect(self.__on_progress_text)
+        self.addContentWidget(self.__progress_view)
+
+    def generator(self):
+        return self.__generator
+
+    @Slot(str)
+    def appendProgress(self, text):
+        self.__progress_view.appendPlainText(text)
+
+    @Slot()
+    def setFinished(self):
+        pass
+
+    @Slot()
+    def setstarted(self):
+        pass
+
+    @Slot()
+    def __on_progress_text(self):
+        self.__progress_view.verticalScrollBar().setValue(self.__progress_view.verticalScrollBar().maximum())
