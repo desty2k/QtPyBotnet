01ef0b157af47bcc09c62ecb9c0aef6d8fbe2bf7
desty2k <55806660+desty2k@users.noreply.github.com>
2021-05-11 12:22:29 +0200

    UPD: validate config before saving it
    ADD: signals for sending config to GUI clients

--- QtPyBotnet/core/config.py
+++ QtPyBotnet/core/config.py
@@ -1,29 +1,38 @@
 import os
 import json
+import psutil
+import socket
 import logging
+import ipaddress
 
 from qtpy.QtCore import Signal, QObject, Slot
 
-from core.crypto import encrypt, decrypt
+from core.crypto import generate_key
 from core.importer import get_subclassess_by_name, function_importer
 
 
 class ConfigManager(QObject):
     """Manages and loads config files."""
-    config_created = Signal()
-    config_error = Signal(str)
+    config_missing = Signal()
     config_read = Signal(dict)
-    config_read_error = Signal(str)
+
+    config_get = Signal(int, dict)
+    config_saved = Signal(int)
+    config_error = Signal(int, str)
+    validation_error = Signal(int, str)
 
     available_tasks = Signal(int, list)
     available_infos = Signal(int, list)
+    
+    setup_options = Signal(int, dict)
 
     def __init__(self):
         super(ConfigManager, self).__init__(None)
         self.__config = {}
         self.__key = ""
-        self.__logger = logging.getLogger(self.__class__.__name__)
-        self.__config_path = os.path.expanduser("~\\QtPyBotnet.cfg")
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.__config_path = ".\\QtPyBotnet.cfg"
+        self.__indent = 2
 
     def setValue(self, key, value):
         if self.__config_path:
@@ -33,19 +42,50 @@ class ConfigManager(QObject):
 
     def value(self, key):
         if self.__config_path:
-            return self.__config.get(key)
+            return self.__config[key]
         else:
             raise Exception("Config not loaded")
 
-    def sync(self):
-        with open(self.__config_path, "w+") as f:
-            data = json.dumps(self.__config)
-            data = encrypt(data.encode(), self.__key.encode()).decode()
-            f.write(data)
-            self.__logger.info("Config written in {}".format(self.__config_path))
+    @Slot(int, dict)
+    def save(self, device_id, config: dict):
+        try:
+            self.logger.info("Recevied config save request from {}: {}".format(device_id, config))
+            ipaddress.ip_address(config.get("gui_ip"))
+            ipaddress.ip_address(config.get("c2_ip"))
+        except Exception as e:
+            self.logger.warning("Config validation failed")
+            self.validation_error.emit(device_id, str(e))
+            return
+
+        try:
+            with open(self.__config_path, "w+") as f:
+                data = json.dumps(config, indent=self.__indent)
+                f.write(data)
+            self.config_saved.emit(device_id)
+            self.logger.info("Config written in {}".format(self.__config_path))
+        except Exception as e:
+            self.config_error.emit(device_id, "Failed to create config: {}".format(e))
+
+    @Slot(int)
+    def get(self, device_id):
+        self.config_get.emit(device_id, self.__config)
+
+    @Slot(int)
+    def load(self) -> dict:
+        if self.config_exists():
+            data = open(self.__config_path, "r").read().encode()
+            try:
+                self.__config = json.loads(data)
+                return self.__config
+            except json.JSONDecodeError:
+                self.config_error.emit("Config is not valid JSON.")
+            except Exception as e:
+                self.config_error.emit("Failed to decrypt config file: {}".format(e))
+        else:
+            self.config_missing.emit()
 
     def config_exists(self):
-        return os.path.exists(self.__config_path) and os.path.isfile(self.__config_path)
+        return os.path.isfile(self.__config_path)
 
     def delete_config(self):
         if self.config_exists():
@@ -60,7 +100,7 @@ class ConfigManager(QObject):
         return function_importer("./client/", "infos")
 
     @Slot(int)
-    def on_gui_client_get_tasks(self, device_id):
+    def on_gui_server_get_tasks(self, device_id):
         tasks = []
         for task in self.get_tasks():
             tasks.append({"name": task.__name__, "platforms": task.platforms,
@@ -68,37 +108,29 @@ class ConfigManager(QObject):
         self.available_tasks.emit(device_id, tasks)
 
     @Slot(int)
-    def on_gui_client_get_infos(self, device_id):
+    def on_gui_server_get_infos(self, device_id):
         self.available_infos.emit(device_id, self.get_infos())
 
-    def create(self, config, key: str):
-        try:
-            with open(self.__config_path, "w+") as f:
-                data = json.dumps(config)
-                data = encrypt(data.encode(), key.encode()).decode()
-                f.write(data)
-            self.config_created.emit()
-            self.__logger.info("Config written in {}".format(self.__config_path))
-        except Exception as e:
-            self.config_error.emit("Failed to create config: {}".format(e))
+    @Slot(int)
+    def on_gui_server_get_setup_options(self, device_id):
+        resp = {}
+        # keys
+        resp["c2_key"] = generate_key().decode()
+        resp["gui_key"] = generate_key().decode()
 
-    def load(self, key: str) -> dict:
-        key = key.encode()
-        if self.config_exists():
-            data = open(self.__config_path, "r").read().encode()
-            try:
-                data = decrypt(data, key).decode()
-                if data:
-                    self.__config = json.loads(data)
-                    self.config_read.emit(self.__config)
-                    return self.__config
-                else:
-                    self.config_error.emit("Invalid config file.")
-            except json.JSONDecodeError:
-                self.config_error.emit("Config is not valid JSON.")
-            except ValueError:
-                self.config_error.emit("Invalid decryption key!")
-            except Exception as e:
-                self.config_error.emit("Failed to decrypt config file: {}".format(e))
-        else:
-            self.config_error.emit("Could not find config file.")
+        # network interfaces
+        interfaces = []
+        for interface, snics in psutil.net_if_addrs().items():
+            mac = None
+            for snic in snics:
+                if snic.family == -1:
+                    mac = snic.address
+                if snic.family == 2:
+                    if mac:
+                        interfaces.append({"ip": snic.address,
+                                           "netmask": snic.netmask,
+                                           "name": interface,
+                                           "mac": mac})
+        resp["interfaces"] = interfaces
+        self.setup_options.emit(device_id, resp)
+
