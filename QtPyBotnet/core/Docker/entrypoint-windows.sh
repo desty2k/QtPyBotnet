7e387f35b61cf4901b17d16ee06a7f082702dd0c
desty2k <55806660+desty2k@users.noreply.github.com>
2021-05-13 17:03:00 +0200

    ADD: show workdir after building Docker image

--- QtPyBotnet/core/Docker/entrypoint-windows.sh
+++ QtPyBotnet/core/Docker/entrypoint-windows.sh
@@ -11,6 +11,7 @@ set -x
 # Useful for CI pipiles which use docker for their build steps
 # and don't allow that much flexibility to mount volumes
 WORKDIR=${SRCDIR:-/src}
+echo $WORKDIR
 
 #
 # In case the user specified a custom URL for PYPI, then use
