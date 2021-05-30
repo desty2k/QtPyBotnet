5a36ec2a351bca1dadc0bcca0a33a07f8228d884
desty2k <wojciech.wentland@int.pl>
2021-05-30 14:53:59 +0200

    ADD: logging level constant in client config file

--- QtPyBotnet/client/config.py
+++ QtPyBotnet/client/config.py
@@ -3,6 +3,7 @@ C2_TIMEOUT = 5
 C2_HOSTS = [{"ip": "127.0.0.1", "port": 8192},
             {"ip": "192.168.10.54", "port": 8192}]
 C2_ENCRYPTION_KEY = b""
+LOGGING_LEVEL = 60
 
 ALLOW_VM = False
 ALLOW_DOCKER = False
